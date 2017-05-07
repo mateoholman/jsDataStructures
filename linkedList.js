86f8d4280eb2627757c789c999ad16e790faf5c5
Matthew Holman <mateoholman@gmail.com>
2017-05-06 22:52:09 -0500

    Added hashTable

--- linkedList.js
+++ linkedList.js
@@ -1,1 +1,23 @@
 //Coding a linked list in JavaScript
+
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, prev, next) {
+  this.value = value;
+  this.prev = prev;
+  this.next = next;
+}
+
+LinkedList.prototype.addToHead = function () {
+  const newNode = new Node(value, this.head, null);
+  if(this.head) {
+    this.head.prev = newNode;
+  }
+  else {
+    this.tail = newNode;
+  }
+  this.head = newNode;
+};
